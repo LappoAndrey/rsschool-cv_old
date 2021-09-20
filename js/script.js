b8518808bcdb1c808c04235e044103c414edd257
silent-pp <silent34@yandex.ru>
2021-09-20 23:33:56 +0300

    refactor: change jQuery tu JS of the project
    refactor: change jQuery tu JS of the project

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
-$(document).ready(function(){
-    $('.header__burger').click(function(event){
-        $('.header__burger,.header__menu').toggleClass('active');
-        $('body').toggleClass('lock');
+const showBurger = document.querySelector('.header__body');
+if (showBurger){
+    const headBurger = document.querySelector('.header__burger');
+    const headMenu = document.querySelector('.header__menu');
+    showBurger.addEventListener("click", function (e){
+        document.body.classList.toggle('lock')
+        showBurger.classList.toggle('active');
+        headBurger.classList.toggle('active');
+        headMenu.classList.toggle('active');
+        
     });
-});
+}
+
